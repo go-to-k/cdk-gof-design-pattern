32caa06384c1498c0872955de29501423f283952
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-04-20 13:37:34 +0900

    update AdapterStack

--- lib/stack/adapter/adapter-stack.ts
+++ lib/stack/adapter/adapter-stack.ts
@@ -8,37 +8,35 @@ export class AdapterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AdapterConfigStackProps) {
     super(scope, id, props);
 
+    const policyStatements = this.createPolicyStatements();
+
+    // カスタムメソッド付きバケット生成
     const bucketAdapter = new BucketAdapter(this, "bucket-adapter");
-
-    // 何かConstructの戻り値とか外部パタメータなどから持って来たイメージ
-    const policyStatements = getPolicyStatements();
-
     bucketAdapter.addManyToResourcePolicy(policyStatements);
   }
-}
 
-// 再現のためのテキトー関数
-const getPolicyStatements = () => {
-  return [
-    new PolicyStatement({
-      effect: Effect.DENY,
-      principals: [new ArnPrincipal("*")],
-      actions: ["s3:PutObject"],
-      conditions: {
-        StringLike: {
-          "aws:Referer": ["http://www.abc.example.com/*"],
+  private createPolicyStatements(): PolicyStatement[] {
+    return [
+      new PolicyStatement({
+        effect: Effect.DENY,
+        principals: [new ArnPrincipal("*")],
+        actions: ["s3:PutObject"],
+        conditions: {
+          StringLike: {
+            "aws:Referer": ["http://www.abc.example.com/*"],
+          },
         },
-      },
-    }),
-    new PolicyStatement({
-      effect: Effect.DENY,
-      principals: [new ArnPrincipal("*")],
-      actions: ["s3:GetObject"],
-      conditions: {
-        StringLike: {
-          "aws:Referer": ["http://www.cde.example.com/*"],
+      }),
+      new PolicyStatement({
+        effect: Effect.DENY,
+        principals: [new ArnPrincipal("*")],
+        actions: ["s3:GetObject"],
+        conditions: {
+          StringLike: {
+            "aws:Referer": ["http://www.cde.example.com/*"],
+          },
         },
-      },
-    }),
-  ];
-};
+      }),
+    ];
+  }
+}
