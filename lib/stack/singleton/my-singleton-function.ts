68f297142145d582db6f5dbc8895ca18800ab336
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-05-05 02:04:38 +0900

    update singleton

---
+++ lib/stack/singleton/my-singleton-function.ts
@@ -0,0 +1,21 @@
+import { NodejsFunction, NodejsFunctionProps } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+
+export class MySingletonFunction extends NodejsFunction {
+  private static singleton: MySingletonFunction;
+
+  private constructor(scope: Construct, id: string, props: NodejsFunctionProps) {
+    super(scope, id, props);
+  }
+
+  static getInstance(
+    scope: Construct,
+    id: string,
+    props: NodejsFunctionProps,
+  ): MySingletonFunction {
+    if (this.singleton == undefined) {
+      this.singleton = new MySingletonFunction(scope, id, props);
+    }
+    return this.singleton;
+  }
+}
--- lib/stack/singleton/singleton-stack.ts
+++ lib/stack/singleton/singleton-stack.ts
@@ -3,15 +3,22 @@ import { Construct } from "constructs";
 import { SingletonConfigStackProps } from "./config";
 import { Code, Runtime, SingletonFunction } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
+import { MySingletonFunction } from "./my-singleton-function";
 
 // トップディレクトリで`yarn build-singleton`してからデプロイ
 export class SingletonStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: SingletonConfigStackProps) {
     super(scope, id, props);
 
+    // AWS提供のSingletonFunction
     // 何度呼んでも一つしか作られない
     this.createLambda("A");
     this.createLambda("B");
+
+    // 自作のSingletonFunction
+    // 何度呼んでも一つしか作られない
+    this.createMyLambda("A");
+    this.createMyLambda("B");
   }
 
   private createLambda(suffix: string): SingletonFunction {
@@ -20,6 +27,15 @@ export class SingletonStack extends cdk.Stack {
       code: Code.fromAsset(join(__dirname, "./lambda")),
       handler: "index.handler",
       runtime: Runtime.NODEJS_18_X,
+    });
+  }
+
+  private createMyLambda(suffix: string): MySingletonFunction {
+    return MySingletonFunction.getInstance(this, `MySingleton${suffix}`, {
+      entry: join(__dirname, "./lambda/index.ts"),
+      bundling: {
+        forceDockerBundling: false,
+      },
     });
   }
 }
