e940683bb9ed73ec717d8e9ddfe4b3b5295c4d47
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-05-06 03:06:49 +0900

    update for decorator comments

--- lib/stack/decorator/removal-policy-resource-decorator.ts
+++ lib/stack/decorator/removal-policy-resource-decorator.ts
@@ -4,12 +4,15 @@ import { Construct } from "constructs";
 export abstract class RemovalPolicyResourceDecorator extends Resource {
   private resource: Resource;
 
+  // constructorも拡張していると言える
   constructor(scope: Construct, id: string, resource: Resource) {
     super(scope, id);
     this.resource = resource;
     this.applyRemovalPolicy(this.getOwnRemovalPolicy());
   }
 
+  // この例では実質このメソッドは拡張していないが
+  // 本来この中の挙動を変えたりなどの拡張ができる
   applyRemovalPolicy(policy: RemovalPolicy): void {
     this.resource.applyRemovalPolicy(policy);
   }
