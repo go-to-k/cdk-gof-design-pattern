ad4995da90dfb5a5b5dbcb6488ee307d3b7c8c39
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-05-12 22:03:45 +0900

    update: aspects.ts

--- lib/stack/visitor/aspect.ts
+++ lib/stack/visitor/aspect.ts
@@ -2,6 +2,7 @@ import { Annotations, IAspect, Tokenization } from "aws-cdk-lib";
 import { CfnBucket } from "aws-cdk-lib/aws-s3";
 import { IConstruct } from "constructs";
 
+// 公式より: https://docs.aws.amazon.com/cdk/v2/guide/aspects.html
 export class BucketVersioningChecker implements IAspect {
   public visit(node: IConstruct): void {
     // See that we're dealing with a CfnBucket
