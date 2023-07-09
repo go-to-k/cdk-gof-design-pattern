8ac689c2e6da05e27512024a7aae76bce8a88506
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-07-09 17:31:32 +0900

    update: convert to english for Decorator pattern

--- lib/stack/decorator/removal-policy-resource-decorator.ts
+++ lib/stack/decorator/removal-policy-resource-decorator.ts
@@ -4,15 +4,15 @@ import { Construct } from "constructs";
 export abstract class RemovalPolicyResourceDecorator extends Resource {
   private resource: Resource;
 
-  // constructorも拡張していると言える
+  // Extended constructor
   constructor(scope: Construct, id: string, resource: Resource) {
     super(scope, id);
     this.resource = resource;
     this.applyRemovalPolicy(this.getOwnRemovalPolicy());
   }
 
-  // この例では実質このメソッドは拡張していないが
-  // 本来この中の挙動を変えたりなどの拡張ができる
+  // In this example, this method is not extended in effect,
+  // but it can be extended to change the behavior of this method, etc.
   applyRemovalPolicy(policy: RemovalPolicy): void {
     this.resource.applyRemovalPolicy(policy);
   }
